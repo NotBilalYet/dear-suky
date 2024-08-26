d792192206f7f79122cfd282e761d678accb00df
Bilal Ado <1bilalado@gmail.com>
2024-08-26 23:22:30 +0100

    adjectives array + finite loop after fade + touchstart trigger

--- app.js
+++ app.js
@@ -1,9 +1,50 @@
 const fadeSvg = document.querySelector('.fade');
 
-document.addEventListener('click', () => {
-    fadeSvg.classList.add('faded');
+const adjectives = [
+    'Amazing.',
+    'Beautiful.',
+    'Radiant.',
+    'Lovely.',
+    'Elegant.',
+    'Adorable.',
+    'Magical.',
+    'Serene.',
+    'Gorgeous.', 
+    'Breathtaking.',
+    'Graceful.',
+    'Luminous.',
+    'Gentle.',
+    'Caring.',
+    'Sweet.',
+    'Heartwarming.',
+    'Consistent.',
+    'Kind.',
+    'Soothing.',
+    'Cute.',
+    'Patient.',
+    'Sincere.'
+];
+
+function displayAdjectives() {
+    let index = 0;
     
-    setTimeout(() => {
-        fadeSvg.remove(); 
-    }, 500);
+    function showNextAdjective() {
+        if (index < adjectives.length) {
+            document.getElementById('adjective').innerText = adjectives[index++];
+            setTimeout(showNextAdjective, 1000);
+        }
+    }
+    
+    showNextAdjective();
+}
+
+
+function startFadeAndAdjectives() {
+    fadeSvg.classList.add('faded');
+    displayAdjectives();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.addEventListener('click', startFadeAndAdjectives);
+    document.body.addEventListener('touchstart', startFadeAndAdjectives);
 });
