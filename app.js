cddd058550654e526ad7474f427c98dfb1f5d88d
Bilal Ado <1bilalado@gmail.com>
2024-08-28 16:18:38 +0100

    fixed opacity issue with visibility + no borders

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const box1 = document.getElementById('box-1');
 const box2 = document.getElementById('box-2');
+const box3 = document.getElementById('box-3');
 
 const adjectives = [
     'Amazing.',
@@ -28,23 +29,35 @@ const adjectives = [
 
 function displayAdjectives() {
     let index = 0;
-    
+
     function showNextAdjective() {
         if (index < adjectives.length) {
             document.getElementById('adjective').innerText = adjectives[index++];
             setTimeout(showNextAdjective, 1000);
         } else {
             box2.classList.add('faded');
-            setTimeout(createHearts, 500);
+            setTimeout(() => {
+                box2.style.opacity = 0;
+                setTimeout(() => {
+                    box3.style.opacity = 1;
+                    createHearts();
+                }, 500);
+            }, 500);
         }
     }
-    
+
     showNextAdjective();
 }
 
 function startFadeAndAdjectives() {
     box1.classList.add('faded');
-    displayAdjectives();
+    setTimeout(() => {
+        box1.style.opacity = 0;
+        setTimeout(() => {
+            box2.style.opacity = 1;
+            displayAdjectives();
+        }, 500);
+    }, 500);
 }
 
 function createHeart() {
